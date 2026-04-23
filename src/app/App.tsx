54020d22bae64c9be546d37b45d582010b413a80
shivam <shivamvrewa@gmail.com>
2026-04-23 14:12:01 +0530

    remove mouse scroll animation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -178,20 +178,7 @@ export default function App() {
           </motion.div>
         </div>
 
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1 }}
-          className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
-        >
-          <div className="w-6 h-10 border-2 border-amber-500/50 rounded-full flex justify-center pt-2">
-            <motion.div
-              animate={{ y: [0, 12, 0] }}
-              transition={{ duration: 1.5, repeat: Infinity }}
-              className="w-1.5 h-1.5 bg-amber-500 rounded-full"
-            ></motion.div>
-          </div>
-        </motion.div>
+
       </section>
 
       {/* Featured Products */}
